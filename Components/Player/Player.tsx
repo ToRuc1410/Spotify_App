d229bb31c65218d9f8311fd903ad41fec70bfc13
Tờ Rúc <ngotruc917@gmail.com>
2023-08-17 20:43:27 +0700

    update

--- Components/Player/Player.tsx
+++ Components/Player/Player.tsx
@@ -72,11 +72,11 @@ export default function Player() {
   }
 
   return (
-    <div className=' flex h-20 my-2 bg-gradient-to-b from-slate-900 to-black  text-xs md:text-base lg:text-base px-2 '>
+    <div className='grid grid-cols-12 h-20 my-2 bg-gradient-to-b from-slate-900 to-black  text-xs md:text-base lg:text-base px-2 '>
       {/* Left Player : now Information Song */}
-      <div className='flex flex-row items-center'>
+      <div className='col-span-4 flex flex-row items-center'>
         {selectedSong && (
-          <div className='flex justify-start items-center '>
+          <div className='flex justify-center items-center '>
             <Image
               src={selectedSong.album.images[0].url}
               height={50}
@@ -100,7 +100,7 @@ export default function Player() {
       </div>
 
       {/* Playing song */}
-      <div className=' flex space-x-2 md:space-x-4 lg:space-x-4 justify-center py-2 items-center'>
+      <div className='col-span-4 flex space-x-2 md:space-x-4 lg:space-x-4 justify-center py-2 items-center'>
         <svg
           xmlns='http://www.w3.org/2000/svg'
           fill='none'
@@ -200,7 +200,7 @@ export default function Player() {
       </div>
 
       {/* Volume of Song */}
-      <div className=' flex justify-center items-center space-x-2'>
+      <div className='col-span-4 flex justify-center items-center space-x-2'>
         <svg
           xmlns='http://www.w3.org/2000/svg'
           fill='none'
